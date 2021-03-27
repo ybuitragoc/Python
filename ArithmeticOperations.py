420b6533e977d49ab36e39806903a2a2566736e3
Andrea Buitrago <ybuitragoc@gmail.com>
2021-03-26 19:13:30 -0500

    new add file

---
+++ ArithmeticOperations.py
@@ -0,0 +1,31 @@
+## Type of data
+
+##integer / entero
+e = 5
+
+##Real number / float / decimal
+real = 0.759
+
+#Explonente base 10 / Base exponent 10
+real_b = 0.759e-3 
+print (real_b)
+
+#Operaciones aritmeticas / Arithmetic operations
+
+#SUMA / SUM:
+print(e+real)
+
+#Resta / SUBTRACTION
+print(real-e)
+
+#Multiplicacion / Multiplication
+print(real*e)
+
+#Exponente - Elevear al cuadrado / Exponent - Square
+print(e**2)
+
+#Division
+print(float(e)/real)
+
+#Modulo / MOD
+print(7%3)

67a81cb722bd62ef0df76e63066cad8f24b59d24
Andrea Buitrago <ybuitragoc@gmail.com>
2021-03-27 19:58:10 -0500

    new file diccionarios

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+#Diccionarios / dictionaries
+dic = {'clave1':[1,2,3],'clave2': True}
+print(dic)
+
+#Imprimir elmento de la lista de la primer clave del diccionario / #Print item from the list of the first dictionary key
+print(dic['clave1'][0])
+
+#Se pueden modificar las claves de los diccionarios /#You can modify the keys of the dictionaries
+dic ['clave1'] = False
+print(dic['clave1'])

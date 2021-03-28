d90f4670cbfd9053cecd7dd640f49f30ce0a4ff2
Andrea Buitrago <ybuitragoc@gmail.com>
2021-03-27 19:51:02 -0500

    new file truplas

---
+++ truplas.py
@@ -0,0 +1,9 @@
+#Tuplas / tuple
+t = (2 , "text", True)
+print(type(t))
+
+#acceder elemento de la tupla / access tuple element
+print(t[1])
+
+#¿se puede modificar el elemento de una tupla? --> No, esta es la diferente con una lista
+# Can the element of a tuple be modified? -> No, this is the different one with a list

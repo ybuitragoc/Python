346f0b542cc069597265bc7dd3866baa1c2dcef0
Andrea Buitrago <ybuitragoc@gmail.com>
2021-03-27 20:07:56 -0500

    new file logic

---
+++ relational_operators.py
@@ -0,0 +1,34 @@
+#operadores relacionales / relational operators
+
+var_1= 4
+var_2= 5
+var_3 = "Hello"
+var_4 = "Hola"
+#Comparar dos variables "=" / #Compare two variables
+result = var_1 == var_2
+print (result)
+
+#Comparar dos variables "!=" / #Compare two variables
+result = var_1 != var_2
+print (result)
+
+#Variable mayor que / #Variable greater than
+result = var_1 > var_2
+print (result)
+
+#Variable menor que /#Variable less than
+result = var_1 < var_2
+print (result)
+
+#Variable mayor igual que /#Variable greater equal to
+result = var_1 >= var_2
+print (result)
+
+#Variable menor igual que / #Variable less than
+result = var_1 <= var_2
+print (result)
+
+#Comparar dos cadenas / #Compare two strings
+#no se recomiendo usar operadores lógicos para comparar / it is not recommended to use logical operators to compare
+result = var_3 == var_4
+print (result)

a728bf5b2d01bb686252d17ddaa40bac04896f29
Andrea Buitrago <ybuitragoc@gmail.com>
2021-03-29 18:58:35 -0500

    new file

---
+++ conditional_statements.py
@@ -0,0 +1,13 @@
+#Sentencias condicionales / conditional statements
+edad = 14
+m_edad = 18
+
+if edad >= m_edad:
+    print ("Eres mayor de edad")
+    if False:
+        print  ("Esto se ejecuta siempre que sea mayr de edad")
+    else:
+        print("Cualquier cosa")
+else:
+    print("No eres mayor de edad") 
+

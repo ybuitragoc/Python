7e3510efcfbc2ace851e0fe92fa2f9412f11b438
Andrea Buitrago <ybuitragoc@gmail.com>
2021-03-29 20:53:36 -0500

    new file

--- herencia.py
+++ herencia.py
@@ -11,15 +11,25 @@ class Human:
         print(mensaje, "I'm", self.nombre)
 
 #created  new class
-    
+#heredar una clase
+class SystEng(Human):
+    def developer(self,code):
+        print("I'm goint to programming in", code)
+ #heredar una clase   
+class Lowyer(Human):
+    def studyCase(self,de):
+        print("I must study", de)
+
+
+   
 #create object
-object_1 = Human(18, 'Colombia', 'Pedro')
-object_2 = Human(39, 'Peru', 'Raul')
+object_1 = SystEng(18, 'Colombia', 'Pedro')
+object_2 = Lowyer(39, 'Peru', 'Raul')
 
 #print atribute
 print("I'm ",object_1.nombre, "and I'm " , object_1.edad, " Years old, and I'm from ", object_1.pais)
 print("I'm ",object_2.nombre, "and I'm " , object_2.edad, " Years old, and I'm from ", object_2.pais)
 
-#method
-object_1.toTalk("Hi")
-object_2.toTalk("Hi")
+#cada objeto comparte los atributos de clase Humano, pero tiene sus funciones 
+object_1.developer("Python")
+object_2.studyCase("laws")
